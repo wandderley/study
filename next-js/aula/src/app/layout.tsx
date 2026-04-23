19aa1513ffbfb694f4e69cbb442a0af6e8ad1468
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2026-04-22 22:11:21 -0300

    nextJs: metadata

--- next-js/aula/src/app/layout.tsx
+++ next-js/aula/src/app/layout.tsx
@@ -1,6 +1,26 @@
 import "./globals.css";
 
-import {Header} from "../components/header"
+import { Header } from "../components/header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Aula Next Js do Zero!",
+  description: "Aprendendo Next Js do zero como Sujeito Programador!",
+  openGraph: {
+    title: "Aprendendo Next Js com Sujeoto no Youtube",
+    description: "Aprendendo Next Js do zero como Sujeito Programador!",
+    // images: ['url here']
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
@@ -8,15 +28,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`antialiased`}
-    >
+    <html lang="en" className={`antialiased`}>
       <body className="min-h-full flex flex-col">
         <Header />
-        
+
         {children}
-        </body>
+      </body>
     </html>
   );
 }
--- next-js/aula/src/app/page.tsx
+++ next-js/aula/src/app/page.tsx
@@ -1,3 +1,23 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home - Aula Next Js do Zero!",
+  description: "Aprendendo Next Js do zero como Sujeito Programador!",
+  openGraph: {
+    title: "Aprendendo Next Js com Sujeoto no Youtube",
+    description: "Aprendendo Next Js do zero como Sujeito Programador!",
+    // images: ['url here']
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+};
 
 export default function Home() {
   return (
