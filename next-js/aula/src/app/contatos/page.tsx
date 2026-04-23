ddddfb7c8f6244bbf6c49ff70f47a36a04a3c51e
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2026-04-22 21:58:40 -0300

    NeXtjS: entendendo estruturas e navegação de páginas

---
+++ next-js/aula/src/app/contatos/page.tsx
@@ -0,0 +1,7 @@
+export default function Contatos() {
+    return(
+        <div>
+            <h1>Página Contatos</h1>
+        </div>
+    )
+}
---
+++ next-js/aula/src/components/header/index.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="flex px-4 py-4 bg-gray-800 text-white">
+      <div className="flex items-center justify-between w-full mx-auto max-w-7x1">
+        <div>NextJS</div>
+        <nav>
+          <ul className="flex items-center justify-center gap-2">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+
+            <li>
+              <Link href="/posts">Posts</Link>
+            </li>
+
+            <li>
+              <Link href="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
